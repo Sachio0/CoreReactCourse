44cff7ca9c9d982beaa84caa2ea41982a412d1eb
nowi.king <Nowi.King@gmail.com>
2020-04-05 14:26:40 +0200

    that

---
+++ client-app/src/App.tsx
@@ -0,0 +1,22 @@
+import React, {Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { cars } from './demo';
+import CarItem from './CarItem';
+
+class App extends Component {
+    
+    render() {
+        return (
+            <div className="App">
+                <ul>
+                    {cars.map((car) =>
+                        <CarItem car={car} />
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ client-app/src/demo.ts
@@ -0,0 +1,20 @@
+﻿let data: number | string;
+data = "43";
+export interface ICar{
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1: ICar = {
+    color: 'blue',
+    model: 'bmw'
+}
+const car2: ICar = {
+    color: 'red',
+    model: 'merceders',
+    topSpeed: 100
+}
+const multiply = (x: number, y: number) => (x * y)
+
+export const cars = [ car1, car2 ];
